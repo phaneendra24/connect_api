e0ff94817a044c79f7c5e2ec197b42d24620e3d1
phaneendra <phaneendrapilli777@gmail.com>
2023-04-16 15:29:10 +0530

    cors error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/api/tweets", tweet);
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://connect-ruddy.vercel.app/"],
+    origin: ["https://connect-ruddy.vercel.app/"],
   },
 });
 
